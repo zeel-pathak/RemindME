459c1c140867076692cf73781b776db11a2d4693
Zeel Pathak <zeelpathak2018.it@gmail.com>
2021-08-30 21:38:05 +0530

    Added view engines
    Made some routes and some modifications for the upcoming features

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 let express = require('express');
 let app = express();
+var path = require('path');
 
 var homeRouter = require('./routes/home');
 
 app.use('/', homeRouter);
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(3000);
 console.log("Server is listining ar port 3000");
