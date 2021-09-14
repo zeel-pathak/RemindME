fe9fbe491f5dcd515996fa01e3e064d14d7aa783
Zeel Pathak <zeelpathak2018.it@gmail.com>
2021-09-14 19:08:18 +0530

    Added firestore with the app

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 let express = require('express');
 let app = express();
 var path = require('path');
-
 var homeRouter = require('./routes/home');
 
 app.use('/', homeRouter);
@@ -12,6 +11,19 @@ app.set('view engine', 'ejs');
 
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+//Database Connection 
+
+var fs = require("firebase-admin");
+
+var serviceAccount = require("./serviceAccountKey.json");
+
+fs.initializeApp({
+  credential: fs.credential.cert(serviceAccount),
+  databaseURL: process.env.url
+});
+
 
 app.listen(3000);
 console.log("Server is listining ar port 3000");
